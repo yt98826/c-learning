92999a461e19b1ba6bdc5c303a98643553639a65
leon98826 <yt988261314@163.com>
2026-07-18 07:53:22 +0800

    补充完善第一个c语言程序

--- day01/demo01.c
+++ day01/demo01.c
@@ -4,11 +4,21 @@ int main()
 {
 	printf("hello world\n");
 	printf("hi leon\n");
+	//printf('haha');		//报错，但是前面的代码会执行，这个后面的不会，''只能包裹单个字符
 	printf("%d\n", 500);
-	//printf('haha');
 	printf("%d\n", 100);
-	printf("%c\n", 's');	
+	printf("%c\n", "ab");
+	printf("%s\n", "ab");
+	printf("%c\n", 's');
+	printf("%f\n",2.546);	// %f 默认输出 6 位小数
+	printf("%4f\n",2.5465614891);	// %.nf n替换为数字可以控制小数位数
 	return 0;
 }
 
-//我是一个注释
+//部分占位符解释
+/*
+%d  有符号十进制整数
+%c  单个字符
+%s	字符串（以 \0 结尾的字符数组）
+%f	十进制小数形式的 float 或 double（默认输出 6 位小数）
+*/
