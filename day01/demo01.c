a04883bae1d0d81777228ccb49c10ed8b4da61e6
leon98826 <yt988261314@163.com>
2026-07-17 18:53:02 +0800

    day01

--- day01/demo01.c
+++ day01/demo01.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 	printf("hello world\n");
+	printf("hi leon\n");
+	printf("%d\n", 500);
+	//printf('haha');
+	printf("%d\n", 100);
+	printf("%c\n", 's');	
 	return 0;
 }
+
+//我是一个注释
