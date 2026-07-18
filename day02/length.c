32232efbca7e33d2368555ce6ce47f91569ae8d7
leon98826 <yt988261314@163.com>
2026-07-18 15:48:00 +0800

    day02一半

---
+++ day02/length.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("%zu\n", sizeof(char));
+	return 0;
+}
--- day02/test.c
+++ day02/test.c
@@ -1,7 +1,12 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main()
 {
-	printf("hi\n");
+	_Bool flag = true;		//false or true
+	//bool flag = true;		//也可以使用 bool 这样定义，这个在文件定义过了，相当于起了个别名
+	if (flag)
+		printf("I love c\n");
+
 	return 0;
 }
